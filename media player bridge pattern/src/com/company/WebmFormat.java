e81ef0c708e6f0fc4046a0b5835fe8db4c4ca3d8
Maksat <75685615+1August@users.noreply.github.com>
2021-10-20 14:43:40 +0600

    Delete media player bridge pattern directory

--- media player bridge pattern/src/com/company/WebmFormat.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class WebmFormat implements MediaFormat{
-    @Override
-    public void readMedia() {
-        System.out.println("Webm format is reading!");
-    }
-}
